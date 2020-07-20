f55a620bd382801e6cab1df01d5cc7d40fc1294e
sunhao1234567 <2421308903@qq.com>
2020-07-20 16:27:23 +0800

    Test

---
+++ app/src/main/java/com/example/hw/activitys/LoginInActivity.java
@@ -0,0 +1,17 @@
+package com.example.hw.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.hw.R;
+
+public class LoginInActivity extends AppCompatActivity {
+    Button bt3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
